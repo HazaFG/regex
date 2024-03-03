c113f23260e47e53b71e6e0987966ad4b151f05e
albertomartinez <marioalbec21_@alu.uabcs.mx>
2024-03-02 20:15:51 -0700

    Formato y corrección de errores.

--- src/Main.java
+++ src/Main.java
@@ -17,17 +17,17 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
 
-        //Variable para controlar la ejecucion del programa
-        boolean programa = false;
-
-        //Ciclo para mantener la ejecucion del programa
-        while(!programa){
-            // Crea objeto Scanner para leer la entrada del usuario
-            Scanner scanner = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
             // Solicita al usuario que ingresar texto
             System.out.print("Ingrese un texto alfanumérico largo: ");
-            String textoIngresado = scanner.nextLine();
+            StringBuilder textoIngresado = new StringBuilder();
+
+            //Lee lineas de texto hasta que aparezca una en blanco
+            String linea;
+            while (!(linea = scanner.nextLine()).isEmpty()) {
+                textoIngresado.append(linea).append("\n");
+            }
 
             // Expresión regular para identificar números válidos
             String regex = "\\b(\\d+([,.]\\d+([\\w+.]\\d*)?)?)\\b%?";
@@ -36,35 +36,30 @@ public class Main {
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(textoIngresado);
 
-            // Buscar y mostrar los números encontrados con su tipo
+            //Buscar y mostrar los números encontrados con su tipo
             int i = 1;
+            System.out.printf("| %-4s | %-10s | %-15s | %-10s%n", "No°", "No Fila", "Cadena", "Tipo");
+
             while (matcher.find()) {
                 String numero = matcher.group();
-                int lineMatch = obtenerLineaCoincidente(textoIngresado, matcher.start());
+                int lineMatch = obtenerLineaCoincidente(textoIngresado.toString(), matcher.start());
                 String tipo = determinarTipo(numero);
-                System.out.printf("\n%d - Línea %d - %s - %s\n", i, lineMatch, numero, tipo);
+
+                System.out.printf("| %-4d | %-10d | %-15s | %-10s%n", i, lineMatch, numero, tipo);
+
                 i++;
             }
-
-            // Imprime el texto ingresado
-            //System.out.println("Texto ingresado: " + textoIngresado);
 
             //Decision del usuario si salir o no
             System.out.print("\nDeseas ejecutar otra sentencia? 1-SI, 2-NO: ");
             int decision = scanner.nextInt();
+            scanner.nextLine();
 
-            //SI ES IGUAL A 1 EL PROGRAMA SE VA A CERRAR
-            if (decision == 1){
-                programa = false;
-            }else {
-                //SALIR
-                programa = true;
-
-                // Cierra el scanner
-                scanner.close();
+            //Si es igual a 2, salir del bucle infinito
+            if (decision == 2) {
+                break;
             }
         }
-
     }
 
     //Metodo para determinar la linea donde se encuentre la coinicidencia
@@ -78,7 +73,7 @@ public class Main {
         return numeroLinea;
     }
 
-    // Método para determinar el tipo de número
+    //Método para determinar el tipo de número
     private static String determinarTipo(String numero) {
         if (numero.matches("\\b\\d+[,.]\\d+\\b")){
             return "Numero real";
@@ -98,5 +93,4 @@ public class Main {
 
         return "Numero invalido";
     }
-
 }
