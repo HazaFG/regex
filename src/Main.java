dc71eb3832293f5e3aac20c7a7221c18682fb106
Haza Pro <hazaelfg@gmail.com>
2024-03-02 17:25:52 -0700

    Decision del programa

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Variable para controlar la ejecucion del programa
+        boolean programa = false;
+
+        //Ciclo para mantener la ejecucion del programa
+        while(!programa){
+            // Crea objeto Scanner para leer la entrada del usuario
+            Scanner scanner = new Scanner(System.in);
+
+            // Solicita al usuario que ingresar texto
+            System.out.print("Ingrese un texto alfanumérico largo: ");
+            String textoIngresado = scanner.nextLine();
+
+            // Imprime el texto ingresado
+            System.out.println("Texto ingresado: " + textoIngresado);
+
+
+
+            //Decision del usuario si salir o no
+            System.out.print("Deseas ejecutar otra sentencia? 1-SI, 2-NO: ");
+            int decision = scanner.nextInt();
+
+            //SI ES IGUAL A 1 EL PROGRAMA SE VA A CERRAR
+            if (decision == 1){
+                programa = false;
+            }else {
+                //SALIR
+                programa = true;
+
+                // Cierra el scanner
+                scanner.close();
+            }
+        }
         
-        // Crea objeto Scanner para leer la entrada del usuario
-        Scanner scanner = new Scanner(System.in);
 
-        // Solicita al usuario que ingresar texto
-        System.out.print("Ingrese un texto alfanumérico largo: ");
-        String textoIngresado = scanner.nextLine();
 
-        // Imprime el texto ingresado
-        System.out.println("Texto ingresado: " + textoIngresado);
 
-        // Cierra el scanner
-        scanner.close();
+
+
     }
 
 
