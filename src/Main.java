c5c243e7ad399e3c100ae5842f790d023b1bd1db
Andrea Lucero <alucerotrasvina@gmail.com>
2024-03-02 17:15:39 -0700

    Entrada de texto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+// Ingenieria en Desarrollo de Software
+// Programacion de Sistemas
+// IDS 6 TM
+// 02/03/2024
+
+// Integrantes:
+// Mario Alberto Castellanos Martinez
+// Hazael Flores Gastelum 
+// Ricardo Garayzar Ortega
+// Andrea Lucero Trasviña
+// Miguel Angel Lazcano Ortega
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        // Crea objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita al usuario que ingresar texto
+        System.out.print("Ingrese un texto alfanumérico largo: ");
+        String textoIngresado = scanner.nextLine();
+
+        // Imprime el texto ingresado
+        System.out.println("Texto ingresado: " + textoIngresado);
+
+        // Cierra el scanner
+        scanner.close();
     }
+
+
 }
