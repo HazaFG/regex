437acf3211c01247c4c8aa70eb08bf9f14e06f5b
albertomartinez <marioalbec21_@alu.uabcs.mx>
2024-03-10 23:42:45 -0700

    Tablas constantes e identificadores

--- src/Fase2.java
+++ src/Fase2.java
@@ -43,7 +43,7 @@ public class Fase2 {
 
             // Tabla 1
             int i = 1;
-            System.out.printf("| %-4s | %-10s | %-15s | %-10s | %-10s%n", "No°", "Linea", "TOKEN", "Tipo", "Código");
+            System.out.printf("TABLA LÉXICA\n| %-4s | %-10s | %-15s | %-10s | %-10s%n", "No°", "Linea", "TOKEN", "Tipo", "Código");
 
             // Buscar y mostrar tokens encontrados con su tipo y código
             i = buscarYMostrarTokens(textoIngresado.toString(),
@@ -56,12 +56,21 @@ public class Fase2 {
                             + "\\b[a-zA-Z_][a-zA-Z0-9_]*\\b", i);
 
             //TABLA CONSTANTES EJEMPLO (PRUEBA)
+            System.out.printf("\nTABLA DE CONSTANTES\n| %-4s | %-10s | %-4s | %-10s%n", "No°", "Constante", "Tipo", "Valor");
             for(int j = 0; j < tokens.size(); j++){
                 if(tokens.get(j).getTipo() == 6){
-                    System.out.print(tokens.get(j).getNombre());
+                    System.out.printf("| %-4s | %-10s | %-4s | %-10s%n", tokens.get(j).getId(),
+                            tokens.get(j).getNombre(), tokens.get(j).getTipo(), tokens.get(j).getValor());
                 }
             }
 
+            //TABLA IDENTIFICADORES EJEMPLO (PRUEBA)
+            System.out.printf("\nTABLA DE IDENTIFICADORES\n| %-17s | %-5s | %-10s%n", "Identificador", "Valor", "Linea");
+            for(int j = 0; j < tokens.size(); j++){
+                if(tokens.get(j).getTipo() == 4){
+                    System.out.printf("| %-17s | %-5s | %-10s%n", tokens.get(j).getNombre(), tokens.get(j).getValor(), tokens.get(j).getNoLinea());
+                }
+            }
 
             System.out.print("\n¿Quieres repetir el proceso? (1: Sí, 2: No): ");
 
@@ -94,6 +103,7 @@ public class Fase2 {
             TipoValor tipoValor = determinarTipoYValor(token);
             System.out.printf("| %-4s | %-10s | %-15s | %-10s | %-10s%n", i, lineMatch, token, tipoValor.getTipo(),
                     tipoValor.getValor());
+            tokens.add(new Token(i, lineMatch, token, tipoValor.getTipo(), tipoValor.getValor()));
             i++;
         }
 
@@ -115,106 +125,72 @@ public class Fase2 {
     private static TipoValor determinarTipoYValor(String palabra) {
         switch (palabra.toUpperCase()) {
             case "SELECT":
-                tokens.add(new Token(palabra, 1, 10));
                 return new TipoValor(1, 10);
             case "FROM":
-                tokens.add(new Token(palabra, 1, 11));
                 return new TipoValor(1, 11);
             case "WHERE":
-                tokens.add(new Token(palabra, 1, 12));
                 return new TipoValor(1, 12);
             case "IN":
-                tokens.add(new Token(palabra, 1, 13));
                 return new TipoValor(1, 13);
             case "AND":
-                tokens.add(new Token(palabra, 1, 14));
                 return new TipoValor(1, 14);
             case "OR":
-                tokens.add(new Token(palabra, 1, 15));
                 return new TipoValor(1, 15);
             case "CREATE":
-                tokens.add(new Token(palabra, 1, 16));
                 return new TipoValor(1, 16);
             case "TABLE":
-                tokens.add(new Token(palabra, 1, 17));
                 return new TipoValor(1, 17);
             case "CHAR":
-                tokens.add(new Token(palabra, 1, 18));
                 return new TipoValor(1, 18);
             case "NUMERIC":
-                tokens.add(new Token(palabra, 1, 19));
                 return new TipoValor(1, 19);
             case "NOT":
-                tokens.add(new Token(palabra, 1, 20));
                 return new TipoValor(1, 20);
             case "NULL":
-                tokens.add(new Token(palabra, 1, 21));
                 return new TipoValor(1, 21);
             case "CONSTRAINT":
-                tokens.add(new Token(palabra, 1, 22));
                 return new TipoValor(1, 22);
             case "KEY":
-                tokens.add(new Token(palabra, 1, 23));
                 return new TipoValor(1, 23);
             case "PRIMARY":
-                tokens.add(new Token(palabra, 1, 24));
                 return new TipoValor(1, 24);
             case "FOREIGN":
-                tokens.add(new Token(palabra, 1, 25));
                 return new TipoValor(1, 25);
             case "REFERENCES":
-                tokens.add(new Token(palabra, 1, 26));
                 return new TipoValor(1, 26);
             case "INSERT":
-                tokens.add(new Token(palabra, 1, 27));
                 return new TipoValor(1, 27);
             case "INTO":
-                tokens.add(new Token(palabra, 1, 28));
                 return new TipoValor(1, 28);
             case "VALUES":
-                tokens.add(new Token(palabra, 1, 29));
                 return new TipoValor(1, 29);
             case ",":
-                tokens.add(new Token(palabra, 5, 50));
                 return new TipoValor(5, 50);
             case ".":
-                tokens.add(new Token(palabra, 5, 51));
                 return new TipoValor(5, 51);
             case "(":
-                tokens.add(new Token(palabra, 5, 52));
                 return new TipoValor(5, 52);
             case ")":
-                tokens.add(new Token(palabra, 5, 53));
                 return new TipoValor(5, 53);
             case "‘":
-                tokens.add(new Token(palabra, 5, 54));
                 return new TipoValor(5, 54);
             case "+":
-                tokens.add(new Token(palabra, 7, 70));
                 return new TipoValor(7, 70);
             case "-":
-                tokens.add(new Token(palabra, 7, 71));
                 return new TipoValor(7, 71);
             case "*":
-                tokens.add(new Token(palabra, 7, 72));
                 return new TipoValor(7, 72);
             case "/":
-                tokens.add(new Token(palabra, 7, 73));
                 return new TipoValor(7, 73);
             case ">":
-                tokens.add(new Token(palabra, 8, 81));
                 return new TipoValor(8, 81);
             case "<":
-                tokens.add(new Token(palabra, 8, 82));
                 return new TipoValor(8, 82);
             case "=":
-                tokens.add(new Token(palabra, 8, 83));
                 return new TipoValor(8, 83);
             case ">=":
-                tokens.add(new Token(palabra, 8, 84));
                 return new TipoValor(8, 84);
             case "<=":
-                tokens.add(new Token(palabra, 8, 85));
                 return new TipoValor(8, 85);
             default:
                 break;
@@ -222,28 +198,30 @@ public class Fase2 {
 
         // Identificar constantes alfanuméricas
         if (palabra.matches("'\\w+'")) {
-            tokens.add(new Token(palabra, 6, obtenerCodigoConstante(palabra, constantes)));
             return new TipoValor(6, obtenerCodigoConstante(palabra, constantes));
         }
 
         // Identificar constantes numéricas
         if (palabra.matches("\\b\\d+\\b")) {
-            tokens.add(new Token(palabra, 6, obtenerCodigoConstante(palabra, constantes)));
             return new TipoValor(6, obtenerCodigoConstante(palabra, constantes));
         }
 
         // Identificar identificadores
         if (palabra.matches("\\b[a-zA-Z_]\\w*\\b")) {
-            tokens.add(new Token(palabra, 4, obtenerCodigoConstante(palabra, constantes)));
             return new TipoValor(4, obtenerCodigoConstante(palabra, identificadores));
         }
 
         return new TipoValor(0, 0);
     }
-
     private static int obtenerCodigoConstante(String constante, Map<String, Integer> mapa) {
         if (!mapa.containsKey(constante)) {
-            mapa.put(constante, constante.startsWith("'") ? valorConstante++ : valorIdentificador++);
+            if (constante.matches("'\\w+'")) {
+                mapa.put(constante, valorConstante++);
+            } else if (constante.matches("\\b\\d+\\b")) {
+                mapa.put(constante, valorConstante++);
+            } else {
+                mapa.put(constante, valorIdentificador++);
+            }
         }
         return mapa.get(constante);
     }
--- src/Token.java
+++ src/Token.java
@@ -1,23 +1,35 @@
 public class Token {
+    private int id;
+    private int noLinea;
     private String nombre;
     private int valor;
     private int tipo;
 
-    public Token (String nombre, int tipo, int valor){
+    public Token (int id, int noLinea, String nombre, int tipo, int valor){
+        this.id = id;
+        this.noLinea = noLinea;
         this.nombre = nombre;
         this.tipo = tipo;
         this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNoLinea() {
+        return noLinea;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public int getValor() {
-        return valor;
-    }
-
     public int getTipo() {
         return tipo;
+    }
+
+    public int getValor() {
+        return valor;
     }
 }
